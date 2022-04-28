ea40f933f1a8bf39793c2068d481a97adffac723
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-04-29 01:07:29 +0200

    ex4

---
+++ js/checkout.js
@@ -0,0 +1,14 @@
+// Get the input fields
+const password = document.querySelector(".password");
+const phone = document.querySelector(".phone");
+const name = document.querySelector(".name");
+
+// Get the error elements
+const errorPassword = document.querySelector("#errorPassword");
+const errorName = document.querySelector("#errorName");
+const errorPhone = document.querySelector("#errorPhone");
+
+// Exercise 6
+function validate() {
+  // Validate fields entered by the user: name, phone, password, and email
+}
