a86136e77183ec34a8439b5ffa8da9530273a738
unknown <Melos@PC-1062636.formacio.local>
2022-04-29 14:27:15 +0200

    ex4/promociones - bueno y barato

--- js/shop.js
+++ js/shop.js
@@ -89,27 +89,18 @@ function calculateTotal(cartList) {
 }
 
 // Exercise 4
-function generateCart() {
-  // recogo los ids de products
-  // const idProduct = []
-  // for (let i = 0; i < products.length; i++) {
-  //   idProduct.push(products[i].id)
-  // }
-
-  // recorrido por el array de objetos cartList
+function generateCart(cart) {
   for (let i = 0; i < products.length; i++) {
-    // filter
     let count = 0
 
     for (let j = 0; j < cartList.length; j++) {
       if (products[i].id == cartList[j].id) {
         count = count + 1
-        console.table('cartList', cartList[i].id,'count', count)
+        console.table('cartList', cartList[i].id, 'count', count)
       }
     }
 
-    // TODO: tengo la cantidad y el id
-    const quantity = { quantity: count}
+    const quantity = { quantity: count }
     const newCartItemQuantity = {
       ...quantity,
       ...products[i],
@@ -118,7 +109,7 @@ function generateCart() {
   }
   // Using the "cartlist" array that contains all the items in the shopping cart,
   // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-  return '✌'
+  return cart
 }
 
 // testing cart
@@ -133,16 +124,57 @@ buy(4)
 buy(4) // 8 items equals
 buy(7)
 buy(7) // 2 items equals
+buy(1)
+buy(1)
+buy(1)
+buy(3)
+buy(3)
+buy(3)
+buy(3)
+buy(3)
+buy(3)
+buy(3)
+buy(3)
+buy(3)
+buy(3)
 
-generateCart()
+generateCart(cart)
 
 console.table('cartList', cartList)
 console.table('cart', cart)
 
 // Exercise 5
-function applyPromotionsCart() {
+function applyPromotionsCart(cart) {
   // Apply promotions to each item in the array "cart"
+  // En aquest exercici has de completar la funció applyPromotionsCart(),
+  //  la qual rep el array cart, i calcula les promocions del carret.
+  // productos en promodicón ids: 1, 3
+
+  for (let i = 0; i < cart.length; i++) {
+    let subtotalPromocionOli, subtotalPromocionPastis
+    // PROMOCIÓN: Si l'usuari compra 3 o més ampolles d'oli, el preu del producte descendeix a 10 euros.
+    if (cart[i].id == 1 && cart[i].quantity >= 3) {
+      console.log(cart[i].price, cart[i].quantity)
+      subtotalPromocionOli = cart[i].quantity * 10
+      console.log('subtotalPromocionOli', subtotalPromocionOli)
+      total += subtotalPromocionOli
+    }
+
+    // PROMOCiON: En comprar-se 10 o més mescles per a fer pastís, el seu preu es rebaixa a 2/3.
+    if (cart[i].id == 3 && cart[i].quantity >= 10) {
+      console.log('funciona')
+      console.log(cart[i].price, cart[i].quantity)
+      subtotalPromocionPastis = cart[i].quantity * cart[i].price * (2 / 3)
+      console.log('subtotalPromocionPastis', subtotalPromocionPastis)
+      total += subtotalPromocionPastis
+    }
+
+
+  }
+  return total
 }
+
+console.log(applyPromotionsCart(cart));
 
 // ** Nivell II **
 
